86d6d18e098b21155afd8ce7f8b899a547ce7915
andriidem308 <andriidem308@gmail.com>
2023-05-13 00:57:45 +0300

    Homework 10 done

--- src/homework10/task_3_2/Main.java
+++ src/homework10/task_3_2/Main.java
@@ -5,21 +5,24 @@ public class Main {
 
     public static void main(String[] args) {
         Beverage espresso = new Espresso();
-        espresso = new MilkDecorator(espresso);
-        espresso = new CreamDecorator(espresso);
         espresso = new SugarDecorator(espresso);
         espresso = new SugarDecorator(espresso);
         System.out.println(espresso);
 
-        Beverage darkRoast = new DarkRoast();
-        darkRoast = new CreamDecorator(darkRoast);
-        darkRoast = new WhippedCreamDecorator(darkRoast);
-        darkRoast = new MilkDecorator(darkRoast);
-        darkRoast = new SugarDecorator(darkRoast);
-        System.out.println(darkRoast);
+        Beverage darkRoast_1 = new DarkRoast();
+        darkRoast_1 = new WhippedCreamDecorator(darkRoast_1);
+        darkRoast_1 = new SugarDecorator(darkRoast_1);
+        darkRoast_1 = new SugarDecorator(darkRoast_1);
+        System.out.println(darkRoast_1);
+
+        Beverage darkRoast_2 = new DarkRoast();
+        darkRoast_2 = new CreamDecorator(darkRoast_2);
+        darkRoast_2 = new SugarDecorator(darkRoast_2);
+        System.out.println(darkRoast_2);
 
         Beverage decaf = new Decaf();
-        decaf = new WhippedCreamDecorator(decaf);
+        decaf = new MilkDecorator(decaf);
+        decaf = new SugarDecorator(decaf);
         decaf = new SugarDecorator(decaf);
         System.out.println(decaf);
     }

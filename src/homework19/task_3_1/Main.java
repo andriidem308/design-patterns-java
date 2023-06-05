2075bf2e560b2ffe648040f1d5a220ebd7ee566c
Andriy Demchenko <andrii.demchencko.c@pricespider.com>
2023-06-05 21:25:24 +0300

    Homework 19 Done

---
+++ src/homework19/task_3_1/Main.java
@@ -0,0 +1,9 @@
+package homework19.task_3_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader("resources/input01.txt");
+    }
+
+}
